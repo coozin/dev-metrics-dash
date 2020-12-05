eda047038cbeeaba3e21c198ade55d476f79622e
coozin <shouppejosh@gmail.com>
2020-12-05 13:03:44 +0100

    changing querried repos back to original 4

--- src/utils/coreRequest.js
+++ src/utils/coreRequest.js
@@ -7,25 +7,19 @@ const utilities = {
   },
   allRepoGroups: {
     "repositories": [
-      "github.com/athenianco/athenian-webapp",
       "github.com/athenianco/athenian-api",
-      "github.com/athenianco/environments",
-      "github.com/athenianco/helm-repo",
+      "github.com/athenianco/athenian-webapp",
       "github.com/athenianco/infrastructure",
       "github.com/athenianco/metadata",
-      "github.com/athenianco/precomputer"
     ],
-    "repogroups": [[0],[1],[2],[3],[4],[5],[6]]
+    "repogroups": [[0],[1],[2],[3]]
   },
   justRepos: {
     "repositories": [
-      "github.com/athenianco/athenian-webapp",
       "github.com/athenianco/athenian-api",
-      "github.com/athenianco/environments",
-      "github.com/athenianco/helm-repo",
+      "github.com/athenianco/athenian-webapp",
       "github.com/athenianco/infrastructure",
       "github.com/athenianco/metadata",
-      "github.com/athenianco/precomputer"
     ],
   }
 }
